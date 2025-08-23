893aa69e39b9b0dc52737dbd1dc19eee8304e89c
bryant tunbutr <bryantt23@users.noreply.github.com>
2025-08-24 05:23:49 +0800

    view & tests

--- lib/view.rb
+++ lib/view.rb
@@ -1,5 +1,16 @@
 class View
   def show_welcome
-    "Welcome to Tic Tac Toe"
+    "Welcome to Tic Tac Toe!"
+  end
+
+  def show_player_turn(player)
+    "Player #{player}, make your move"
+  end
+
+  def show_winner(winner)
+    if winner == :tie
+      return "It was a Tie"
+    end
+    "#{winner} wins!"
   end
 end
--- spec/view_spec.rb
+++ spec/view_spec.rb
@@ -4,7 +4,34 @@ RSpec.describe View do
   describe "#show_welcome" do
     it "returns the welcome message" do
       view = View.new
-      expect(view.show_welcome).to eq("Welcome to Tic Tac Toe")
+      expect(view.show_welcome).to eq("Welcome to Tic Tac Toe!")
+    end
+  end
+  describe "#show_player_turn" do
+    it "returns the prompt for player X" do
+      view = View.new
+      expect(view.show_player_turn("X")).to eq("Player X, make your move")
+    end
+
+    it "returns the prompt for player O" do
+      view = View.new
+      expect(view.show_player_turn("O")).to eq("Player O, make your move")
+    end
+  end
+  describe "#show_winner" do
+    it "returns 'X wins!' when given 'X'" do
+      view = View.new
+      expect(view.show_winner("X")).to eq("X wins!")
+    end
+
+    it "returns 'O wins!' when given 'O'" do
+      view = View.new
+      expect(view.show_winner("O")).to eq("O wins!")
+    end
+
+    it "returns 'It was a Tie' when given :tie" do
+      view = View.new
+      expect(view.show_winner(:tie)).to eq("It was a Tie")
     end
   end
 end
